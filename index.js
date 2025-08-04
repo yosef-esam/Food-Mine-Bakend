905841fce82afa669943b031521bba1ee457163f
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2025-08-04 19:19:54 +0200

    ssss

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-// server.js
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+import FoodRouter from "./src/routers/food.router.js";
+import UserRouter from "./src/routers/user.router.js";
+import { dbconnect } from "./src/config/database.config.js";
+import OrderRouter from "./src/routers/order.router.js";
+import uploadRouter from "./src/routers/upload.router.js";
+
+dbconnect();
+
 const app = express();
-const port = 5000;
+app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://food-hub-rho-silk.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+  res.json({
+    message: "🦄🌈✨👋🌍🌍🌍✨🌈🦄",
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.use("/api/foods", FoodRouter);
+app.use("/api/users", UserRouter);
+app.use("/api/orders", OrderRouter);
+app.use("/api/upload", uploadRouter);
+
+// ✅ Export the Express app for Vercel
+if (process.env.NODE_ENV !== "production") {
+  const port = 5000;
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+}
+
+export default app;
